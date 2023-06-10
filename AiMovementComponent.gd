1a7ce1d90e0342d23cdcb80709d93ce69c8a7e3d
Guest1 <dimezske@gmail.com>
2023-06-10 13:24:50 +1000

    Updates test changes v0.24

--- AiMovementComponent.gd
+++ AiMovementComponent.gd
@@ -16,42 +16,49 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	target = get_node("/root/BattleScene/Player")  # Change "/root/Player" to the path of your player character node
 	randomize()
-	changeDirection()
+#	changeDirection()
 	
 	$Vision/CollisionShape2D.shape.radius = vision_range
-	
-func _process(delta):
-	var distance = target.global_position.distance_to(global_position)
 
-	if distance > attackDistance:
-		get_parent().move_and_collide(direction * speed * delta)
+#func _process(delta):
+#	var distance = target.global_position.distance_to(global_position)
+#
+##    if distance > attackDistance:
+##        get_parent().move_and_collide(direction * speed * delta)
+#	if distance <= attackDistance:
+#		attack()
+#
+#	if chasing:
+#		get_parent().move_and_collide(get_parent().global_position.direction_to(target.global_position) * speed * delta)
+#
+#	timer += delta
+#	if timer >= movementInterval && !chasing:
+#		changeDirection()
+#		timer = 0.0
 
-	if distance <= attackDistance:
-		attack()
-
-	timer += delta
-	if timer >= movementInterval:
-		changeDirection()
-		timer = 0.0
-
-func changeDirection():
-	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+#func changeDirection():
+#	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+#	position.x = clamp(position.x, 0, screen_size.x)
+#	position.y = clamp(position.y, 0, screen_size.y)
 
 func attack():
-	print("Attacking!")
-	get_parent()._attack()
+	if get_parent().attacking == false:
+		# Implement your attack logic here
+	#    target -= attack.attack_damage
+		print("Attacking!")
+		# Add your attack code, such as spawning projectiles or applying damage to the player
+		get_parent()._attack()
+
+#func randomizeTimer():
+#    $AttackTimer.wait_time = randf_range(1,2)
+#    $AiAttackComponent/Hitbox.visible == true
+#    $AiAttackComponent/Hitbox/Collider.disabled = true
+#
+#func _on_attack_timer_timeout():
+#    randomizeTimer()
 	
 
-func randomizeTimer():
-	$AttackTimer.wait_time = randf_range(1,2)
-	$AiAttackComponent/Hitbox.visible == true
-	$AiAttackComponent/Hitbox/Collider.disabled = true
-	
-func _on_attack_timer_timeout():
-	randomizeTimer()
-	
+
 func _on_vision_body_entered(body):
 	if body == target:
 		chasing = true
@@ -60,3 +67,65 @@ func _on_vision_body_entered(body):
 func _on_vision_body_exited(body):
 	if body == target:
 		chasing = false
+#extends Node2D
+#class_name AiMovementComponent
+#
+#@export var speed = 200
+#@export var attackDistance = 100
+#@export var movementInterval = 2  # Time interval for changing movement direction
+#@export var vision_range = 500 # How far the enemy can see.
+#
+#var target: Node2D
+#var timer = 0.0
+#var direction = Vector2.ZERO
+#var screen_size
+#var chasing : bool = false
+#
+#func _ready():
+#	screen_size = get_viewport_rect().size
+#	target = get_node("/root/BattleScene/Player")  # Change "/root/Player" to the path of your player character node
+#	randomize()
+#	changeDirection()
+#
+#	$Vision/CollisionShape2D.shape.radius = vision_range
+#
+#func _process(delta):
+#	var distance = target.global_position.distance_to(global_position)
+#
+#	if distance > attackDistance:
+#		get_parent().move_and_collide(direction * speed * delta)
+#
+#	if distance <= attackDistance:
+#		attack()
+#
+#	timer += delta
+#	if timer >= movementInterval:
+#		changeDirection()
+#		timer = 0.0
+#
+#func changeDirection():
+#	direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+#	position.x = clamp(position.x, 0, screen_size.x)
+#	position.y = clamp(position.y, 0, screen_size.y)
+#
+#func attack():
+#	print("Attacking!")
+#	get_parent()._attack()
+#
+#
+#func randomizeTimer():
+#	$AttackTimer.wait_time = randf_range(1,2)
+#	$AiAttackComponent/Hitbox.visible == true
+#	$AiAttackComponent/Hitbox/Collider.disabled = true
+#
+#func _on_attack_timer_timeout():
+#	randomizeTimer()
+#
+#func _on_vision_body_entered(body):
+#	if body == target:
+#		chasing = true
+#
+#
+#func _on_vision_body_exited(body):
+#	if body == target:
+#		chasing = false
--- Scenes/Snake.gd
+++ Scenes/Snake.gd
@@ -8,9 +8,13 @@ var MAX_HEALTH := 20.0
 var health
 
 var taking_damage := false
+var attacking = false
 
 func _ready():
 	health = MAX_HEALTH
+	var t = get_tree().create_tween()
+	t.tween_property(self, "global_position", $AiMovementComponent.target.global_position + Vector2(randf_range(-30,30), randf_range(-30,30)), 3)
+	t.tween_property(self, "global_position", $AiMovementComponent.target.global_position + Vector2(randf_range(-30,30), randf_range(-30,30)), 3)
 
 func _process(delta):
 	pass #pass animations
@@ -21,6 +25,34 @@ func _on_hitbox_component_input_event(viewport, event, shape_idx):
 func _attack():
 	$Weapon/Hitbox/Collider.disabled = false
 	$Weapon/Hitbox/Collider.visible = true
+	attacking = true
 	await get_tree().create_timer(0.25).timeout
+	attacking = false
 	$Weapon/Hitbox/Collider.visible = false
 	$Weapon/Hitbox/Collider.disabled = true
+#extends Node2D
+#class_name Snake
+#
+#var MonsterAmount := 1
+#var MonsterLevel := 1
+#
+#var MAX_HEALTH := 20.0
+#var health
+#
+#var taking_damage := false
+#
+#func _ready():
+#	health = MAX_HEALTH
+#
+#func _process(delta):
+#	pass #pass animations
+#
+#func _on_hitbox_component_input_event(viewport, event, shape_idx):
+#	print(event)
+#
+#func _attack():
+#	$Weapon/Hitbox/Collider.disabled = false
+#	$Weapon/Hitbox/Collider.visible = true
+#	await get_tree().create_timer(0.25).timeout
+#	$Weapon/Hitbox/Collider.visible = false
+#	$Weapon/Hitbox/Collider.disabled = true
